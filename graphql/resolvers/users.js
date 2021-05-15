fe0bba324edba2ba37c764fa03fe95b44d0a6cfd
cpelichek <cpelichek@gmail.com>
2021-05-15 20:49:22 -0300

    Throw error if username already exist on our database; we are using specific errors from Apollo, this will be useful later because Apollo Client will be able to recognize specific errors and be able to handle it

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
 
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
@@ -14,7 +15,16 @@ module.exports = {
     ) {
       // TODO: Validate user data
       // TODO: Make sure user doesn't already exist
-      // TODO: hash password and create an auth token
+      const user = await User.find({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      }
+
+      // hash password and create an auth token
       password = await bcrypt.hash(password, 12);
 
       const newUser = new User({
