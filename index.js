ae26722d156ae94a4284ebdb970132ea80cf1590
cpelichek <cpelichek@gmail.com>
2021-05-15 18:36:39 -0300

    Connect to server (MongoDB Atlas)

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
+const { MONGODB } = require("./config.js");
+
 const typeDefs = gql`
   type Query {
     sayHi: String!
@@ -22,7 +24,12 @@ const server = new ApolloServer({
 
 // We need to connect we the database before we start our server
 // that's where mongoose comes in
-mongoose.connect();
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    return server.listen({ port: 5000 });
+  });
 
 server.listen({ port: 5000 }).then((res) => {
   console.log(`Server running at ${res.url}`);
