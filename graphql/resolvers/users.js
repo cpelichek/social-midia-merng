2d0dfe72a94e987a78eb3edaa77e31dbba603e45
cpelichek <cpelichek@gmail.com>
2021-05-15 23:57:25 -0300

    Create validateLoginInput to validators.js; create login Mutation to typeDefs.js and users.js; create generateToken helper method to users.js

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,17 +2,59 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
 
-const { validateRegisterInput } = require("../../utils/validators");
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../utils/validators");
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
+// Helper method created to generate our web token; since it's used multiple times we transformed it into the function bellow.
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+}
+
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong user or password";
+        throw new UserInputError("Wrong user or password", { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
     async register(
       _,
-      { registerInput: { username, email, password, confirmPassword } },
-      context,
-      info
+      { registerInput: { username, email, password, confirmPassword } }
     ) {
       // Validate user data
       const { valid, errors } = validateRegisterInput(
@@ -47,15 +89,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res);
 
       return {
         ...res._doc,
@@ -65,7 +99,9 @@ module.exports = {
     },
   },
 };
+// Mutation functions that we create receive 4 arguments: parent, args, context, info
 // Sometimes we will have multiple resolvers and will want to know the result of the last step before the one we are at - so data goes from one resolver them goes to the other, and them the next one, getting processed in different ways and then returned to the user. In that case we would pass "parent" argument before "args". But because in our case we don't have any resolver before this one, "parent" would be undefined, so we can pass "_" instead of "parent" - that way it doesn't take any space.
 // "args" here is the arguments from the typeDefs registerInput
 // "info" will have some metadata
 // But what about "context"?
+// We can omit info and context for now, since they haven't been used yet
