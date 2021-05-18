263988df090ff942212c7ac31173c8d5e79aca1d
cpelichek <cpelichek@gmail.com>
2021-05-17 22:58:32 -0300

    Create deleteComment

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -1,4 +1,4 @@
-const { UserInputError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const Post = require("../../models/Post");
 const checkAuth = require("../../utils/check-auth");
@@ -27,6 +27,33 @@ module.exports = {
         await post.save();
         return post;
       } else throw new UserInputError("Post not found");
+    },
+
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (post) {
+          const commentIndex = post.comments.findIndex(
+            (comment) => comment.id === commentId
+          );
+
+          if (post.comments[commentIndex].username === username) {
+            // splice is, among other things, the correct way to "delete" a "key-value" pair from an array without leaving an empty space
+            post.comments.splice(commentIndex, 1);
+            await post.save();
+            return post;
+          } else {
+            // This is just a safety net, since we'll never show a delete button on a comment that's not owned by that user (it'll, however, prevent a user from trying to delete a comment that's not theirs with some sort of code)
+            throw new AuthenticationError("Action not allowed");
+          }
+        } else {
+          throw new UserInputError("Post not found");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
     },
   },
 };
