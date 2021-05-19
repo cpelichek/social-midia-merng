b3025b58fbfc15be7e2cae939ea95ba582371867
cpelichek <cpelichek@gmail.com>
2021-05-19 18:42:08 -0300

    Add wrapping Container component from Semantic UI React; modify menu items from MenuBar.js to act like a diferent component, in this case Link from react-router-dom, and pass the props that go on the link, in this case the route

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 
 function MenuBar() {
@@ -12,17 +13,23 @@ function MenuBar() {
         name="home"
         active={activeItem === "home"}
         onClick={handleItemClick}
+        as={Link}
+        to="/"
       />
       <Menu.Menu position="right">
         <Menu.Item
           name="login"
           active={activeItem === "login"}
           onClick={handleItemClick}
+          as={Link}
+          to="/login"
         />
         <Menu.Item
           name="register"
           active={activeItem === "register"}
           onClick={handleItemClick}
+          as={Link}
+          to="/register"
         />
       </Menu.Menu>
     </Menu>
