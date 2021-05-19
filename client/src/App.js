68f8b99a56fd850f8068fafc681b7f948cf3f245
cpelichek <cpelichek@gmail.com>
2021-05-19 18:12:47 -0300

    Create Home, Login and Register pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div>
-      <h1>MySocialMidia</h1>
-    </div>
+    <Router>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Router>
   );
 }
 
