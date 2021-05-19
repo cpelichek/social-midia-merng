5f6f8f31ffafb85735182331036d60d217b85580
cpelichek <cpelichek@gmail.com>
2021-05-19 18:31:43 -0300

    Add MenuBar component, a modified version of one of the Menu types from Semantic UI React

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
+import MenuBar from "./components/MenuBar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -10,6 +11,7 @@ import Register from "./pages/Register";
 function App() {
   return (
     <Router>
+      <MenuBar />
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
