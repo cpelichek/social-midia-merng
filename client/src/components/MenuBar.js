c7e867490c57846bf1cdb02b7cc6e617cdba534a
cpelichek <cpelichek@gmail.com>
2021-05-19 18:56:03 -0300

    Fix the activeItem of MenuBar.js using a  ternary operator which sets the state of activeItem to be the same as the page URL substring

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 
 function MenuBar() {
-  const [activeItem, setActiveItem] = useState("");
-
+  const pathname = window.location.pathname;
+  const path = pathname === "/" ? "home" : pathname.substr(1);
+  const [activeItem, setActiveItem] = useState(path);
   const handleItemClick = (e, { name }) => setActiveItem(name);
 
   return (
-    <Menu pointing secondary>
+    <Menu pointing secondary size="massive" color="teal">
       <Menu.Item
         name="home"
         active={activeItem === "home"}
