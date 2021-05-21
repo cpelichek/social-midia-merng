6cd78e75796ad591d0957446a95172c90dd8626b
cpelichek <cpelichek@gmail.com>
2021-05-21 04:28:09 -0300

    Add Login page, which is a modified Register page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form } from "semantic-ui-react";
+import { useMutation } from "@apollo/client";
 
-function Login() {
+import { LOGIN_USER } from "../utils/graphql";
+import { useForm } from "../utils/hooks";
+import styles from "./form.module.css";
+
+function Login(props) {
+  const [errors, setErrors] = useState({});
+
+  const initialState = {
+    username: "",
+    password: "",
+  };
+
+  const { onChange, onSubmit, userValues } = useForm(
+    loginUserCallback,
+    initialState
+  );
+
+  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+    update(_, result) {
+      props.history.push("/");
+    },
+    onError(err) {
+      setErrors(err.graphQLErrors[0].extensions.exception.errors);
+    },
+    variables: userValues,
+  });
+
+  function loginUserCallback() {
+    loginUser();
+  }
+
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className={styles.formContainer}>
+      <Form onSubmit={onSubmit} naValidate className={loading ? "loading" : ""}>
+        <h1>Login</h1>
+        <Form.Input
+          className={styles.required}
+          label="Nome&nbsp;do&nbsp;usuário"
+          placeholder="Seu nome de usuário"
+          name="username"
+          type="text"
+          value={userValues.username}
+          error={
+            errors.username
+              ? {
+                  content: "Por favor, insira o nome do usuário",
+                  pointing: "below",
+                }
+              : false
+          }
+          onChange={onChange}
+        />
+        <Form.Input
+          className={styles.required}
+          label="Senha"
+          placeholder="Sua senha"
+          name="password"
+          type="password"
+          value={userValues.password}
+          error={
+            errors.password
+              ? {
+                  content: "Por favor, insira uma senha válida",
+                }
+              : false
+          }
+          onChange={onChange}
+        />
+        <Button type="submit" primary>
+          Login
+        </Button>
+      </Form>
+      {errors.general === "Wrong user or password" && (
+        <div className="ui error message">
+          <ul className="list">
+            <li>{`Nome do usuário ou senha incorretas`}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
--- client/src/utils/graphql.js
+++ client/src/utils/graphql.js
@@ -49,3 +49,15 @@ export const REGISTER_USER = gql`
     }
   }
 `;
+
+export const LOGIN_USER = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      id
+      email
+      username
+      createdAt
+      token
+    }
+  }
+`;
