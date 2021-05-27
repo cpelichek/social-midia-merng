d4d143b221f1380aaa29e6d1c28ed73360615d4d
cpelichek <cpelichek@gmail.com>
2021-05-27 20:15:42 -0300

    fix(authentication): :pencil2: Correct typo on auth.js
    Had written 'userReducer', when correct is 'useReducer'

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,5 +1,5 @@
 // We are using the context API but we could use Redux for a better data storage and management. Also, we should remember that context doesn't really manage data, what it does is data injection, but that's enough for what we are building in our application.
-import React, { userReducer, createContext } from "react";
+import React, { useReducer, createContext } from "react";
 
 // AuthContext will be used in our components to access our context data
 const AuthContext = createContext({
@@ -30,7 +30,7 @@ function authReducer(state, action) {
 
 // AuthProvider will be used in our App to wrap around our entire application so that it would have access through this provider to the functions in the context
 function AuthProvider(props) {
-  const [state, dispatch] = userReducer(authReducer, { user: null });
+  const [state, dispatch] = useReducer(authReducer, { user: null });
 
   function login(userData) {
     dispatch({
