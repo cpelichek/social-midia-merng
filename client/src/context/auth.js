8e4a67df69f1ad50f2b3186cfa795b568ca5e49f
cpelichek <cpelichek@gmail.com>
2021-05-27 22:04:11 -0300

    feat(authentication): :sparkles: Use localStorage for persistence of user logged in while his token hasn't expired
    Use localStorage for setting, getting and removing user jwt token when they login/register, and use method jwtDecode from jwt-decode package which returns decoded token information like expiration date, which we use to compare with Date.now() to remove token from localStorage if expired or to assign initialState.user to the decoded token information, which will be used by our AuthProvider method replacing {user: null}

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,5 +1,20 @@
 // We are using the context API but we could use Redux for a better data storage and management. Also, we should remember that context doesn't really manage data, what it does is data injection, but that's enough for what we are building in our application.
 import React, { useReducer, createContext } from "react";
+import jwtDecode from "jwt-decode";
+
+const initialState = {
+  user: null,
+};
+
+if (localStorage.getItem("jwtToken")) {
+  const decodedToken = jwtDecode(localStorage.getItem("jwtToken"));
+  // verifies if token expired
+  if (decodedToken.exp * 1000 < Date.now()) {
+    localStorage.removeItem("jwtToken");
+  } else {
+    initialState.user = decodedToken;
+  }
+}
 
 // AuthContext will be used in our components to access our context data
 const AuthContext = createContext({
@@ -30,9 +45,10 @@ function authReducer(state, action) {
 
 // AuthProvider will be used in our App to wrap around our entire application so that it would have access through this provider to the functions in the context
 function AuthProvider(props) {
-  const [state, dispatch] = useReducer(authReducer, { user: null });
+  const [state, dispatch] = useReducer(authReducer, initialState);
 
   function login(userData) {
+    localStorage.setItem("jwtToken", userData.token);
     dispatch({
       type: loginString,
       payload: userData,
@@ -40,6 +56,7 @@ function AuthProvider(props) {
   }
 
   function logout() {
+    localStorage.removeItem("jwtToken");
     dispatch({ type: logoutString });
   }
 
