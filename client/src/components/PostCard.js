812ccef32bb38a560e00e8b6d58f3036e6b2ed68
cpelichek <cpelichek@gmail.com>
2021-06-11 15:25:06 -0300

    feat(userPosts): :sparkles: Create component for LikeButton

--- client/src/components/PostCard.js
+++ client/src/components/PostCard.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Button, Card, Label, Icon, Image } from "semantic-ui-react";
 import moment from "moment";
+
+import { AuthContext } from "../context/auth";
+import LikeButton from "./LikeButton";
 
 const PostCard = ({
   post: {
@@ -11,54 +14,52 @@ const PostCard = ({
     username,
     likeCount,
     commentCount,
-    // likes,
+    likes,
     // comments,
   },
 }) => {
-  function likePost() {
-    console.log("Like post");
-  }
-  function commentOnPost() {
-    console.log("Comment on post");
-  }
+  const { user } = useContext(AuthContext);
 
   return (
-    <div>
-      <Card fluid>
-        <Card.Content>
-          <Image
+    <Card fluid>
+      <Card.Content>
+        <Image
+          floated="right"
+          size="mini"
+          src="https://react.semantic-ui.com/images/avatar/small/jenny.jpg"
+        />
+        <Card.Header>{username}</Card.Header>
+        <Card.Meta as={Link} to={`/posts/${id}`}>
+          {moment(createdAt).fromNow(true)}
+        </Card.Meta>
+        <Card.Description>{body}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <LikeButton user={user} post={{ id, likes, likeCount }} />
+        <Button
+          labelPosition="right"
+          as={Link}
+          to={`/posts/${id}`} // sends user to the page corresponding to the id of the post whose button he clicked - that way we can show the comments on said post in a page of it's own
+        >
+          <Button color="blue" basic>
+            <Icon name="comments" />
+          </Button>
+          <Label basic color="blue" pointing="left">
+            {commentCount}
+          </Label>
+        </Button>
+        {user && user.username === username && (
+          <Button
+            as="div"
+            color="red"
+            onClick={() => console.log("Delete post")}
             floated="right"
-            size="mini"
-            src="https://react.semantic-ui.com/images/avatar/large/molly.png"
-          />
-          <Card.Header>{username}</Card.Header>
-          <Card.Meta as={Link} to={`/posts/${id}`}>
-            {moment(createdAt).fromNow(true)}
-          </Card.Meta>
-          <Card.Description>{body}</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <div>
-            <Button as="div" labelPosition="right" onClick={likePost}>
-              <Button color="teal" basic>
-                <Icon name="heart" />
-              </Button>
-              <Label basic color="teal" pointing="left">
-                {likeCount}
-              </Label>
-            </Button>
-            <Button as="div" labelPosition="right" onClick={commentOnPost}>
-              <Button color="blue" basic>
-                <Icon name="comments" />
-              </Button>
-              <Label basic color="blue" pointing="left">
-                {commentCount}
-              </Label>
-            </Button>
-          </div>
-        </Card.Content>
-      </Card>
-    </div>
+          >
+            <Icon name="times" style={{ margin: 0 }} />
+          </Button>
+        )}
+      </Card.Content>
+    </Card>
   );
 };
 
